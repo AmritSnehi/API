8405ea43e1000ee58a4ca1675da205112f452050
Amrit Snehi <20a.s.0399@gmail.com>
2023-01-13 16:45:38 +0530

    Added entity annotation for home

--- src/main/java/com/springboot/API/model/Home.java
+++ src/main/java/com/springboot/API/model/Home.java
@@ -1,9 +1,14 @@
 package com.springboot.API.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
 public class Home {
+	@Id
+	private String id;
 	private String name;
 	private int noOfRoom;
 	
